9caed0e53beb266d8160de838401531799e448c5
k4boom <mfzenbilci@gmail.com>
2021-11-01 21:52:44 +0300

    Create Response Class

--- core/Application.php
+++ core/Application.php
@@ -4,19 +4,24 @@ namespace app\core;
 
 class Application {
 
+    public static string $ROOT_DIR;
     public Request $request;
     public Router $router;
+    public Response $response;
+    public static Application  $app;
 
-    public function __construct() {
+    public function __construct($root) {
 
+        self::$ROOT_DIR= $root;
+        self::$app = $this;
         $this->request = new Request();
-        $this->router = new Router($this->request);
+        $this->response = new Response();
+        $this->router = new Router($this->request,$this->response);
         
     }
 
     public function run(){
-        //TODO
-        $this->router->resolve();
+        echo $this->router->resolve();
     }
 
 
