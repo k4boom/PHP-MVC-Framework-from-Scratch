bb4e92c3fb09358fe15818e20b5376967751959f
k4boom <mfzenbilci@gmail.com>
2021-11-01 21:52:24 +0300

    View and Layout Rendering

--- core/Router.php
+++ core/Router.php
@@ -7,9 +7,11 @@ class Router {
     protected array $routes = [];
 
     public Request $request;
+    public Response $response;
 
-    public function __construct(Request $request){
+    public function __construct(Request $request,Response $response){
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback){
@@ -22,15 +24,37 @@ class Router {
         $callback = $this->routes[$method][$path] ?? false;
         
         if($callback===false){
-            echo "Not found";
-            exit();
+            $this->response->setStatusCode('404');
+            return "Not found";
+            
         }
-        echo call_user_func($callback);
+        if(is_string($callback)){
+            return $this->renderView($callback);
+        }
+        return call_user_func($callback);
 
         
     }
 
+    public function renderView($view){
+        $layoutContent= $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+        include_once Application::$ROOT_DIR."/views/$view.php";
+    }
 
+    protected function layoutContent(){
+
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+    
+    protected function renderOnlyView($view){
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
+    }
 
 
 }
